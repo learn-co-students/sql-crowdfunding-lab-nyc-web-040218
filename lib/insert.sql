666a52006fe578fa302fc4560efc5e3342efeb8b
github username <github email address>
2018-04-10 11:34:26 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,63 @@
+INSERT INTO users (name, age) VALUES ("Joe", 26);
+INSERT INTO users (name, age) VALUES ("Billy", 34);
+INSERT INTO users (name, age) VALUES ("Matt", 35);
+INSERT INTO users (name, age) VALUES ("Mike", 23);
+INSERT INTO users (name, age) VALUES ("EJ", 26);
+INSERT INTO users (name, age) VALUES ("Ryan", 34);
+INSERT INTO users (name, age) VALUES ("Gui", 28);
+INSERT INTO users (name, age) VALUES ("Shawn", 24);
+INSERT INTO users (name, age) VALUES ("Lane", 25);
+INSERT INTO users (name, age) VALUES ("Brie", 30);
+INSERT INTO users (name, age) VALUES ("Daniel", 30);
+INSERT INTO users (name, age) VALUES ("Edward", 29);
+INSERT INTO users (name, age) VALUES ("Ian", 28);
+INSERT INTO users (name, age) VALUES ("Nicolas", 40);
+INSERT INTO users (name, age) VALUES ("Jesse", 25);
+INSERT INTO users (name, age) VALUES ("Austin", 24);
+INSERT INTO users (name, age) VALUES ("Ahmed", 23);
+INSERT INTO users (name, age) VALUES ("Ry", 24);
+INSERT INTO users (name, age) VALUES ("Shun", 24);
+INSERT INTO users (name, age) VALUES ("Roger", 27);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Make a sick action movie", "art", 1000000, 040218, 090218);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("The next Mac", "technology", 50000, 051018, 103018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Hoodies, but for summertime", "design", 75000, 031518, 081918);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Interpretative Dance", "art", 500, 050118, 063018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("16 handles but w/ alcohol???", "business", 750000, 041018, 070418);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Remember that juice kerig thing? Well, we did that but with french fries", "technology", 600000, 012418, 071618);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Guy Fieri Documentary", "art", 1000, 041718, 061018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Macbook case for napping", "product", 80000, 070418, 090118);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Yo, check this hoverboard out, it totally doesn't catch fire", "technology", 150, 082517, 121018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("You know how they made a Rampage movie? Well, this one is about Turok", "art", 100000000, 081218, 090218);
+
+INSERT INTO pledges (amount, project_id, user_id) VALUES
+(145900, 8, 18),
+(1000000, 5, 2),
+(5000000, 2, 12),
+(8592490, 1, 1),
+(68493000, 3, 5),
+(10, 4, 8),
+(18900, 4, 6),
+(210, 3, 3),
+(3400, 7, 4),
+(67000, 5, 7),
+(5000, 6, 2),
+(70, 10, 20),
+(14, 9, 19),
+(1500, 8, 17),
+(1341314365, 7, 16),
+(1455, 6, 15),
+(141235, 1, 14),
+(88898899, 2, 13),
+(1410, 3, 12),
+(123, 5, 3),
+(489, 8, 3),
+(80, 6, 8),
+(89790, 9, 3),
+(1, 7, 8),
+(10, 8, 4),
+(140, 8, 18),
+(1145, 8, 18),
+(1465, 8, 14),
+(900, 3, 5),
+(0900, 1, 2);
